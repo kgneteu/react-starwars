a389d1442322c4ea73bb5faa5abcbbb70aab4887
kgneteu <77528950+kgneteu@users.noreply.github.com>
2021-11-17 11:51:31 +0100

    neon button added

--- src/neonButton.js
+++ src/neonButton.js
@@ -2,13 +2,12 @@ import classes from './neonButon.module.scss'
 
 const NeonButton = ({children}) => {
     return (
-        <div className={classes.body}>
-            <div className={classes.container}>
+
+            // <div className={classes.container}>
                 <div className={classes.button}>
                     {children}
                 </div>
-            </div>
-        </div>
+            // </div>
     );
 };
 
